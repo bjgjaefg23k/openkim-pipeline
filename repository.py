62513039623463c6a43572f674933c65db59e411
Alex Alemi <alexalemi@gmail.com>
2012-05-13 15:42:01 -0400

    stupid changes

--- repository.py
+++ repository.py
@@ -7,21 +7,21 @@ from contextlib import contextmanager
 #get the repository dir from the symlink
 REPOSITORY_DIR = os.readlink('openkim-repository')
 
-#define a repo_dir context manager
 @contextmanager
 def in_repo_dir():
+    """Change to repo directory to execute code, then change back"""
     cwd = os.getcwd()
     os.chdir(REPOSITORY_DIR)
     yield
     os.chdir(cwd)
 
-
-def pull_repo(remote='origin',branch='master'):
+def pull(remote='origin',branch='master'):
     """ do a git pull """
     with in_repo_dir():
         return subprocess.call(['git','pull',remote,branch])
 
-def push_repo(remote='origin',branch='master'):
+def push(remote='origin',branch='master'):
+    """ do a git push """
     with in_repo_dir():
         return subprocess.call(['git','push',remote,branch])
 
