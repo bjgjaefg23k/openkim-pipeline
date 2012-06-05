d3190424e2a8ccae530f2c2d99abca67d71da7f2
Alex Alemi <alexalemi@gmail.com>
2012-06-05 11:19:53 -0500

    simple template skeleton

--- template.py
+++ template.py
@@ -13,3 +13,11 @@ def get_file(str):
     """ If the str has a file directive, return the filename """
     if str.startswith(KW_FILE):
         pass
+
+def get_modelname(str):
+    """ process a modelname directive """
+
+def process(file):
+    """ takes in a file like object and retuns a processed file like object """
+
+
