2f105f2d2e69555e337f45684ba48007136bf185
Alex Alemi <alexalemi@gmail.com>
2012-06-07 11:30:28 -0500

    working on alemibox

--- stupid.py
+++ stupid.py
@@ -4,7 +4,8 @@ from repository import *
 test = 'TE_333333333333_000'
 model = 'MO_607867530928_000'
 
+guy = ('TE_000000000014_000','MO_607867530916_000')
 
 import runner
-foo = valid_match(test,model)
+foo = valid_match(*guy)
 print foo
