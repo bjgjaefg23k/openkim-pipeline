4c651cfe5f34fea8bc8e6049def6f50cd15adb79
Alex Alemi <alexalemi@gmail.com>
2012-06-08 13:32:43 -0500

    maybe working with long names

--- kimid.py
+++ kimid.py
@@ -15,7 +15,7 @@ which is of the form
 
 """
 
-from persistentdict import PersistentDict
+from persistentdict import PersistentDict, PersistentDefaultDict
 import random, sys
 from config import *
 import repository
@@ -28,9 +28,10 @@ The dictionary of ids is a layered dictionary, or leader, then id then version
 
 NUM_DIGITS = 12
 VERSION_DIGITS = 3
+KIM_NAME_FORMAT = "{}__{}"
 KIM_ID_FORMAT = "{}_{}_{:03d}"
 STORE_FILENAME = KIMID_STORE
-ALLOWED_LEADERS = ["MO","MD","ME","TE","TD","PR","TR","RD"]
+ALLOWED_LEADERS = ["MO","MD","ME","TE","TD","PR","TR","RD","VC","VR"]
 FORMAT = "json"
 
 def randints():
@@ -45,7 +46,7 @@ def next_int(collection):
 
 def get_new_id(leader):
     """ Generate a new KIM ID """
-    with PersistentDict(STORE_FILENAME, format=FORMAT) as store:
+    with PersistentDefaultDict(STORE_FILENAME, format=FORMAT) as store:
         pk = next_int(store[leader])
         version = 0
         store[leader][pk] = version
@@ -56,7 +57,7 @@ def get_new_id(leader):
 def get_new_version(leader,pk):
     """ Get the next version number """
     pk = str(pk)
-    with PersistentDict(STORE_FILENAME, format=FORMAT) as store:
+    with PersistentDefaultDict(STORE_FILENAME, format=FORMAT) as store:
         version = store[leader][pk]
         version += 1
         store[leader][pk] = version
@@ -68,7 +69,7 @@ def get_new_version(leader,pk):
 def get_current_version(leader,pk):
     """ Get the current version number """
     pk = str(pk)
-    with PersistentDict(STORE_FILENAME, format=FORMAT, flag='r') as store:
+    with PersistentDefaultDict(STORE_FILENAME, format=FORMAT, flag='r') as store:
         version = store[leader][pk]
     return version
 
@@ -83,17 +84,28 @@ def promote_kimid(kid):
     return kid
 
 
-def format_kimid(leader,pk,version):
+def format_kimid(leader,pk,version,front=None):
     if isinstance(pk,int):
         pk = "{:08d}".format(pk)
-    return KIM_ID_FORMAT.format(leader,pk,version)
 
-def parse_kimid(kimid):
+    kid = KIM_ID_FORMAT.format(leader,pk,version)
+    if front:
+        return KIM_NAME_FORMAT.format(front,kid)
+    return kid
+
+def parse_kimid(kim_name):
     """given an id return a tuple of its parts"""
-    parts = kimid.split("_")
+    try:
+        #see if we have a front
+        front, back = kim_name.split("__")
+    except ValueError:
+        #looks like we don't have a front 
+        front = None
+        back = kim_name
+    parts = back.split("_")
     leader, pk, version = parts
     version = int(version)
-    return (leader,pk,version)
+    return (front,leader,pk,version)
 
 def new_kimid(leader,pk=None):
     """ Generate a new kim id, if only the leader is given generate a new id number,
@@ -112,7 +124,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "init":
             logger.info("Generating empty store in {}".format(STORE_FILENAME))
-            with PersistentDict(STORE_FILENAME, format=FORMAT) as store:
+            with PersistentDefaultDict(STORE_FILENAME, format=FORMAT) as store:
                 for leader in ALLOWED_LEADERS:
                     store[leader] = {}
 
