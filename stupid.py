9f30035fe4823ba536126043fd471381007286e0
Alex Alemi <alexalemi@gmail.com>
2012-06-12 15:02:31 -0500

    with some changes

--- stupid.py
+++ stupid.py
@@ -4,8 +4,7 @@ from repository import *
 test = 'TE_333333333333_000'
 model = 'MO_607867530928_000'
 
-guy = ('TE_000000000014_000','MO_607867530916_000')
+guy = ("LatticeConstantCubicFccAl__TE_000000000159_000",'IMD_AlMnPd__MO_265192693592_000')
 
-import runner
-foo = valid_match(*guy)
+foo = valid_match(*guy, force=True)
 print foo
