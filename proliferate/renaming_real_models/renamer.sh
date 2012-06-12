c32e1fac48f688b84648c86d392ba7dc4293a9f6
Matt Bierbaum <matt.bierbaum@gmail.com>
2012-06-12 09:40:46 -0500

    new models

--- proliferate/renaming_real_models/renamer.sh
+++ proliferate/renaming_real_models/renamer.sh
@@ -3,7 +3,7 @@ rm -r MODEL*
 rm -r ~/openkim-repository/md/*
 rm -r ~/openkim-repository/mo/*
 
-tar zxvf kim_models.tgz
+tar zxvf kim_models_edits.tgz
 python rename.py
 mv MODEL_DRIVERs/* ~/openkim-repository/md/
 mv MODELs/* ~/openkim-repository/mo/
