b5e5a123505e909132ae6dec6cde6a9d2cf5de6c
Alex Alemi <alexalemi@gmail.com>
2012-06-15 10:00:57 -0500

    with some simple fixes

--- stupid.py
+++ stupid.py
@@ -6,5 +6,7 @@ model = 'MO_607867530928_000'
 
 guy = ("LatticeConstantCubicFccAl__TE_000000000159_000",'IMD_AlMnPd__MO_265192693592_000')
 
+guy = ("LatticeConstantCubicFccSi__TE_000000000276_000","kcc_model_T_SW__MO_609019931472_000")
+
 foo = valid_match(*guy, force=True)
 print foo
