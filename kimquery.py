ace9b0b5765c9085a1c178d3c8619baf60643732
Matt Bierbaum <matt.bierbaum@gmail.com>
2013-06-25 13:01:10 -0500

    query to mongo

--- kimquery.py
+++ kimquery.py
@@ -3,6 +3,7 @@ import urllib2
 import simplejson
 import itertools
 from config import PipelineQueryError
+import simplejson
 
 def query(querydata, queryrules="", keys=None):
     # set up the globals for how to interact with the website
@@ -30,4 +31,24 @@ def query(querydata, queryrules="", keys=None):
     if keys:
         result = [ { k:v for k,v in itertools.izip(keys, elem) } for elem in arr ]
         return result 
+    return arr
+
+def query_mongo(query, url=""):
+    # set up the globals for how to interact with the website
+    url = url or 'http://pipeline.openkim.org/api'
+    user_agent = "OpenKIM Pipeline (http://pipeline.openkim.org/)"
+    header = {'User-Agent' : user_agent, "Content-type": "application/x-www-form-urlencoded"}
+
+    # encode, send, and read the response
+    data = urllib.urlencode(query)
+    request  = urllib2.Request(url, data, header)
+    response = urllib2.urlopen(request)
+    answer = response.read()
+    response.close()
+
+    if not answer:
+        raise PipelineQueryError("No response")
+
+    # we got back JSON, let's convert and apply labels if requested
+    arr = simplejson.loads(answer)
     return arr
